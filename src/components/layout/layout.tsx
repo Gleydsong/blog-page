fb77de72da4761571a4aa26885c783e6dff87d0d
Gleydsong <ggleydson@icloud.com>
2026-03-29 20:11:10 +0100

    style: configure and apply global Inter and PT Sans fonts to headings and layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,3 +1,4 @@
+import { Inter, PT_Sans_Caption } from "next/font/google";
 import { Footer } from "../footer";
 import { Header } from "../header";
 
@@ -5,9 +6,22 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  variable: "--font-inter",
+});
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-sans",
+});
+
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="font-inter relative flex min-h-screen flex-col bg-gray-800">
+    <div
+      className={`${inter.className} ${ptSansCaption.className} relative flex min-h-screen flex-col bg-gray-800`}
+    >
       <Header />
 
       <main className="flex flex-1 flex-col bg-gray-800 mb-12">{children}</main>
